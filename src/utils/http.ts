850e5d6b5a355dd7852e8f23c4de6f39b0b0c804
guanCompany <934750771@qq.com>
2023-03-07 11:26:25 +0800

    fix: 解决 axios 返回报错

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
 
+declare module 'axios' {
+  interface AxiosInstance {
+    (config: AxiosRequestConfig): Promise<any>
+  }
+}
+
 const http = axios.create({
   baseURL: '',
 })

ed2daec3390db42dd01237ccb3f2ef6da42903d7
guanCompany <934750771@qq.com>
2023-03-07 09:30:40 +0800

    refactor: 规范 className

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -12,8 +12,8 @@ export default function Menu(props: {
     <ul className={`${style.menu} ${className}`} style={style}>
       {items.map(m => (
         <li
-          className={`${style["menu-item"]} ${
-            selectedKey === m.key ? style["menu-item-selected"] : ""
+          className={`${style.menuItem} ${
+            selectedKey === m.key ? style.menuItemSelected : ""
           }`}
           key={m.key}
           onClick={() => onClick({ key: m.key })}
